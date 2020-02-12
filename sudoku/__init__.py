5f8e83d99e5fbcc8246d8814f9b12afd381d265d
jtschei <johnscheibmeir@gmail.com>
2020-02-11 21:16:43 -0800

    can build and validate sudoku puzzle

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -1,24 +1,61 @@
-class Board:
+class Puzzle:
     def __init__(self):
         self.reset_board()
 
     def reset_board(self):
         self._board = [[0] * 9 for i in range(9)]
 
+    def set_position(self,x:int, y:int, val: int) -> bool:
+        if (not self._is_position_valid(x,y,val)):
+            return False
+        else:
+            self._board[x][y] = val
+            return True
+
+    def is_solved(self) -> bool:
+        for y in range(0,9):
+            for x in range (0,9):
+                if (self._board[x][y] == 0):
+                    return False
+        return True
+
+    def _is_position_valid(self,x:int, y:int, val:int) -> bool:
+        if (val < 1 or val > 9):
+            return False
+        for xr in range(0,9): 
+            if (self._board[xr][y] == val): 
+                return False
+        for yr in range(0,9):
+            if (self._board[x][yr] == val):
+                return False
+        for xb in range(0,3):
+            for yb in range(0,3):
+                if (self._board[int(x/3) * 3 + xb][int(y/3) * 3 + yb] == val):
+                    return False
+        return True
+
     def __repr__(self):
         return str(self._board)
 
     def __str__(self):
-        header : str = "/" + "-" * 35 + "\\" 
-        data : str = "\n".join(map(lambda y: "|" + "|".join(map(lambda n : f" {n} ", self._board[y])) + "|",range(9)))
-        footer : str = "\\" + "-" * 35 + "/" 
-        return "\n".join((header,data,footer))
-
-class Row:
-    def __init__(self):
-        pass
-
+        data = [] # type: List[str]
+        data.append("|" + "-" * 29 + "|")
+        for y in range(0,9):
+            row = "|"
+            for x in range (0,9):
+                row += " " + str(self._board[x][y]) + " "
+                if ((x + 1) % 3 == 0):
+                    row += "|"
+            data.append(row)
+            if ((y + 1) % 3 == 0):
+                data.append("|" + "-" * 29 + "|")
+        return "\n".join(data)
 
 if __name__ == "__main__":
-    b = Board()
+    b = Puzzle()
+    b.set_position(0,0,1)
+    b.set_position(0,1,3)
+    b.set_position(8,0,2)
+    b.set_position(0,8,2)
     print(b)
+    print(f"is solved = {b.is_solved()}")
---
+++ sudoku/tests/test_sudoku.py
@@ -0,0 +1,24 @@
+import unittest
+import sudoku as s
+
+class TestSudoku(unittest.TestCase):
+
+    def test_solve(self):
+        p = s.Puzzle()
+        v: int = 0
+        for x in range(0,9):
+            for y in range(0,9):
+                if (x % 3 == 0 and y == 0):
+                    v = int(x/3)
+                self.assertTrue(p.set_position(x,y,v % 9 + 1))
+                if (not (x == 8 and y == 8)):
+                    self.assertFalse(p.is_solved())
+                v = v+1
+            v = v + 3
+        print()
+        print(p)
+        self.assertTrue(p.is_solved())
+
+
+if __name__ == '__main__':
+    unittest.main()
