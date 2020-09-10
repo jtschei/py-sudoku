22c611a51e78ff95afe51ae6420f0b806ba039f9
jtschei <johnscheibmeir@gmail.com>
2020-09-09 22:29:37 -0700

    misc

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -3,46 +3,65 @@ import random
 import itertools
 from copy import deepcopy
 
+logger = logging.getLogger()
 
 class Puzzle:
     def __init__(self, board=None):
-        logging.debug("creating puzzle")
-        if (board is None):
+        logger.debug("creating puzzle")
+        if board is None:
             self.reset_board()
         else:
-            self._board = deepcopy(board)
+            self.board = deepcopy(board)
 
     def reset_board(self):
-        logging.debug("resetting board")
-        self._board = [[0] * 9 for i in range(9)]
+        logger.debug("resetting board")
+        self.board = [[0] * 9 for i in range(9)]
 
     def initialize_board(self):
-        logging.debug("initializing board")
+        logger.debug("initializing board")
         v: int = 0
         for x in range(0,9):
             for y in range(0,9):
-                if (x % 3 == 0 and y == 0):
+                if x % 3 == 0 and y == 0:
                     v = int(x/3)
                 self.set_position(x,y,v % 9 + 1)
                 v = v+1
             v = v + 3
 
     def shuffle_board(self):
-        logging.debug("shuffling board")
+        logger.debug("shuffling board")
+
+        #decide shift up/down/left/right
+        #get shared points 
+        shift_direction = random.randint(0,4) 
+        to_shift = Puzzle.get_swaps()
+        if shift_direction == 0:
+            self.shift_left(to_shift)
+        elif shift_direction == 1:
+            self.shift_right(to_shift)
+        elif shift_direction == 2:
+            self.shift_up(to_shift)
+        elif shift_direction == 3:
+            self.shift_down(to_shift)
+            pass
+        else:
+            raise ValueError()
+         
+    def dummy(self):
         swap1 = Puzzle.get_swaps()
         swap2 = Puzzle.get_swaps()
         while(len(list(set(swap1) & set(swap2))) > 0):
-            logging.debug("reswapping")
+            logger.debug("reswapping")
             swap2 = Puzzle.get_swaps()
         swaps = list()
         for s in swap1:
             swaps.append(s)
         for s in swap2:
             swaps.append(s)
-        logging.debug(f"swaps={swaps}")
+        logger.debug(f"swaps={swaps}")
         for position_changes in itertools.permutations(swaps,9):
-            print(position_changes)
-            if (self.swap_positions(position_changes)):
+            logger.debug(f"position_changes -> {position_changes}")
+            if self.swap_positions(position_changes):
                 return
         #for c in position_changes:
         #    self.swap_position(c[0][0],c[0][1],c[1][0],c[1][1])
@@ -50,7 +69,7 @@ class Puzzle:
 
     @staticmethod
     def get_swaps():
-        logging.debug("getting swaps")
+        logger.debug("getting swaps")
         spots=list()
         #box0,0
         x00 = random.randint(0,2)
@@ -91,69 +110,91 @@ class Puzzle:
         return spots
 
     def set_position(self,x:int, y:int, val: int) -> bool:
-        if (not self._is_position_valid(x,y,val)):
+        if not self.__is_position_valid(x,y,val):
             return False
         else:
-            self._board[x][y] = val
+            self.board[x][y] = val
             return True
 
+    def shift_left(self, to_swap):
+        logger.debug(f"shifting left -> {to_swap}")
+        # for next three
+        #x1,y1
+        #x2,y2
+        #x3,y3
+        #v1 = self._board[]
+        pass
+
+    def shift_right(self, to_swap):
+        logger.debug(f"shifting right -> {to_swap}")
+        pass
+
+    def shift_down(self, to_swap):
+        logger.debug(f"shifting down -> {to_swap}")
+        pass
+
+    def shift_up(self, to_swap):
+        logger.debug(f"shifting up -> {to_swap}")
+        pass
+
     def swap_positions(self,swaps) -> bool:
-        logging.debug("swapping positions")
-        temp_puzzle = Puzzle(self._board)
+        logger.debug("swapping positions")
+        temp_puzzle = Puzzle(self.board)
         for s in swaps:
-            v1 = temp_puzzle._board[s[0][0]][s[0][1]]
-            v2 = temp_puzzle._board[s[1][0]][s[1][1]]
-            temp_puzzle._board[s[0][0]][s[0][1]] = v1
-            temp_puzzle._board[s[1][0]][s[1][1]] = v2
-            if (temp_puzzle._is_board_valid()):
-                self._board = deepcopy(temp_puzzle._board)
+            section = int(s[1]/3) + int(s[0]/3)
+            v1 = temp_puzzle.board[section][s[0][0]][s[0][1]]
+            v2 = temp_puzzle.board[s[1][0]][s[1][1]]
+            temp_puzzle.board[s[0][0]][s[0][1]] = v1
+            temp_puzzle.board[s[1][0]][s[1][1]] = v2
+            if temp_puzzle.is_board_valid():
+                self.board = deepcopy(temp_puzzle.board)
                 return True
 
     def swap_position(self,x1:int, y1:int, x2:int, y2:int) -> bool:
-        logging.debug("swap position")
-        v1 = self._board[x1][y1]
-        v2 = self._board[x2][y2]
-        self._board[x1][y1] = v2
-        self._board[x2][y2] = v1
-        logging.debug(f"swapping {x1},{y1}:{v1} with {x2},{y2}:{v2}")
-        if (self._is_position_valid(x1,y1,v2) and self._is_position_valid(x2,y2,v1)):
-            logging.debug("swapped posoition")
+        logger.debug("swap position")
+        v1 = self.board[x1][y1]
+        v2 = self.board[x2][y2]
+        self.board[x1][y1] = v2
+        self.board[x2][y2] = v1
+        logger.debug(f"swapping {x1},{y1}:{v1} with {x2},{y2}:{v2}")
+        if self.__is_position_valid(x1,y1,v2) and self.__is_position_valid(x2,y2,v1):
+            logger.debug("swapped posoition")
             return True
         else:
-            self._board[x1][y1] = v1
-            self._board[x2][y2] = v2
+            self.board[x1][y1] = v1
+            self.board[x2][y2] = v2
             return False
 
     def is_solved(self) -> bool:
         for y in range(0,9):
             for x in range (0,9):
-                if (self._board[x][y] == 0):
+                if self.board[x][y] == 0:
                     return False
         return True
 
-    def _is_board_valid(self) -> bool:
+    def is_board_valid(self) -> bool:
         for xy in range(0,9):
-            if (not self._is_position_valid(xy,xy,self._board[xy][xy])):
+            if not self.__is_position_valid(xy,xy,self.board[xy][xy]):
                 return False
         return True
 
-    def _is_position_valid(self,x:int, y:int, val:int) -> bool:
-        if (val < 1 or val > 9):
+    def __is_position_valid(self,x:int, y:int, val:int) -> bool:
+        if val < 1 or val > 9:
             return False
         for xr in range(0,9): 
-            if (self._board[xr][y] == val): 
+            if self.board[xr][y] == val: 
                 return False
         for yr in range(0,9):
-            if (self._board[x][yr] == val):
+            if self.board[x][yr] == val:
                 return False
         for xb in range(0,3):
             for yb in range(0,3):
-                if (self._board[int(x/3) * 3 + xb][int(y/3) * 3 + yb] == val):
+                if self.board[int(x/3) * 3 + xb][int(y/3) * 3 + yb] == val:
                     return False
         return True
 
     def __repr__(self):
-        return str(self._board)
+        return str(self.board)
 
     def __str__(self):
         data = [] # type: List[str]
@@ -161,11 +202,11 @@ class Puzzle:
         for y in range(0,9):
             row = "|"
             for x in range (0,9):
-                row += " " + str(self._board[x][y]) + " "
-                if ((x + 1) % 3 == 0):
+                row += " " + str(self.board[x][y]) + " "
+                if (x + 1) % 3 == 0:
                     row += "|"
             data.append(row)
-            if ((y + 1) % 3 == 0):
+            if (y + 1) % 3 == 0:
                 data.append("|" + "-" * 29 + "|")
         return "\n".join(data)
 
@@ -174,7 +215,7 @@ if __name__ == "__main__":
     b = Puzzle()
     b.initialize_board()
     print(b)
-    b.shuffle_board()
+    b.dummy()
     print(b)
     #b.set_position(0,0,1)
     #b.set_position(0,1,3)
