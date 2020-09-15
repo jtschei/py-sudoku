b2d239161c3f068df38ee064fd580a46df856327
John <johnscheibmeir@gmail.com>
2020-09-14 22:09:15 -0700

    workign on solver

--- sudoku/__init__.py
+++ sudoku/__init__.py
@@ -8,6 +8,9 @@ logger = logging.getLogger()
 
 
 class Puzzle:
+
+    vals = [1,2,3,4,5,6,7,8,9]
+
     def __init__(self, board: [[]] = None):
         logger.debug("creating puzzle")
         if board is None:
@@ -17,7 +20,7 @@ class Puzzle:
 
     @staticmethod
     def build_blank_board() -> [[]]:
-        logger.debug("building initialized board")
+        logger.debug("building blank board")
         return [[0] * 9 for i in range(9)]
 
     @staticmethod
@@ -106,13 +109,8 @@ class Puzzle:
         while(len(list(set(swap1) & set(swap2))) > 0):
             logger.debug("reswapping")
             swap2 = self.get_swaps()
-        swaps = list()
-        for s in swap1:
-            swaps.append(s)
-        for s in swap2:
-            swaps.append(s)
-        self.print_swaps(swaps)
         # swap values
+        self.print_swaps(swap1 + swap2)
         while len(swap1) > 0:
             (x1,y1) = swap1.pop()
             (x2,y2) = swap2.pop()
@@ -121,7 +119,6 @@ class Puzzle:
             self.board[x1][y1] = v2
             self.board[x2][y2] = v1
 
-
     def get_swaps(self):
         """
         pick two positions in box0,0
@@ -129,9 +126,12 @@ class Puzzle:
         the values in the two positions can be switched in each box
         """
         spots=list()
+        # pick a random position in first box and to list of spots
         x00=random.randint(0,2)
         y00=random.randint(0,2)
         spots.append((x00,y00))
+        # find the position where value of prior position matches in remainder
+        # boxes and add to list of spts
         for x in range(0,9):
             for y in range(0,9):
                 if x < 3 and y < 3:
@@ -139,6 +139,74 @@ class Puzzle:
                 if self.board[x][y] == self.board[x00][y00]:
                     spots.append((x,y))
         return spots
+
+    def hide_values(self, n=9):
+        for i in range(n):
+            logger.debug(f"removing {i} of {n}")
+            x = random.randint(0,8)
+            y = random.randint(0,8)
+            self.board[x][y] = 0
+
+    def solve(self) -> bool:
+        for x in range(0,9):
+            for y in range(0,9):
+                if self.board[x][y] == 0:
+                    p = Puzzle.try_solve(self.board, x, y)
+                    if p is not None:
+                        self.board = Puzzle.copy_board(p.board)
+                        print(self)
+                        logger.info("solved")
+                        return True
+        return False
+
+    @staticmethod
+    def get_next_empty(board: [[]], px: int, py:int) -> (bool,int,int):
+        for x in range(0,9):
+            for y in range(0,9):
+                if x < px or y < py:
+                    continue
+                if board[x][y] == 0:
+                    return (True,x,y)
+        return (False, None, None)
+
+    @staticmethod
+    def try_solve(board: [[]], x: int, y:int):
+        logger.debug(f"solving board for {x},{y}")
+        p = Puzzle(board)
+        print(p)
+        xs = p.get_missing_x(y)
+        ys = p.get_missing_y(x)
+        vs = p.get_missing_v(int(x/3),int(y/3))
+        os = list(set(xs) & set(ys) & set(vs))
+
+        logger.debug(f"missing x's = {xs}")
+        logger.debug(f"missing y's = {ys}")
+        logger.debug(f"missing v's = {vs}")
+        logger.debug(f"missing o's = {os}")
+
+        for o in os:
+            p.board[x][y] = o
+            if p.is_solved():
+                return p
+            else:
+                b, nx, ny = Puzzle.get_next_empty(p.board, x, y)
+                if b:
+                    return Puzzle.try_solve(p.board, nx, ny)
+
+    def get_missing_x(self, y: int) -> []:
+        xs = [self.board[x][y] for x in range(0,9)]
+        return list(set(Puzzle.vals) - set(xs))
+
+    def get_missing_y(self, x: int) -> []:
+        ys = [self.board[x][y] for y in range(0,9)]
+        return list(set(Puzzle.vals) - set(ys))
+
+    def get_missing_v(self,box_x: int, box_y:int) -> []:
+        vs = list()
+        for x in range(0,3):
+            for y in range(0,3):
+                vs.append(self.board[box_x * 3 + x][box_y * 3 + y])
+        return list(set(Puzzle.vals) - set(vs))
 
     def __repr__(self):
         return str(self.board)
@@ -175,7 +243,6 @@ class Puzzle:
             if y % 3 == 0:
                 data.append("|" + "-" * 29 + "|")
         print("\n".join(data))
-        
 
 
 if __name__ == "__main__":
@@ -186,13 +253,16 @@ if __name__ == "__main__":
     b.populate_board()
     print(b.is_solved())
     print(b)
-    for x in range(0,2):
+    for x in range(0,10):
         b.shuffle_board()
         print(b.is_solved())
     print(b)
-    b.board[0][8] = 9
+    b.hide_values()
     print(b)
-    print(b.is_solved())
+    b.solve()
+    #b.board[0][8] = 9
+    #print(b)
+    #print(b.is_solved())
     #b.set_position(0,0,1)
     #b.set_position(0,1,3)
     #b.set_position(8,0,2)
